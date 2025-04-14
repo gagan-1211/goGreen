9fd8011ff308ae6c9d08ce95df05eae24ea53b4d
Gagan-1211 <gagan.nadiger1211@gmail.com>
2025-04-14 19:59:42 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import jsonfile from "jsonfile";
 import moment from "moment";
 import simpleGit from "simple-git";
-import random from "random";
 
 const path = "./data.json";
 
-const markCommit = (x, y) => {
+// Function to create a commit on a specific date
+const createCommit = (weeks, days) => {
   const date = moment()
     .subtract(1, "y")
     .add(1, "d")
-    .add(x, "w")
-    .add(y, "d")
+    .add(weeks, "w")
+    .add(days, "d")
     .format();
 
   const data = {
@@ -18,23 +18,66 @@ const markCommit = (x, y) => {
   };
 
   jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date }).push();
+    simpleGit().add([path]).commit(date, { "--date": date });
   });
 };
 
-const makeCommits = (n) => {
-  if(n===0) return simpleGit().push();
-  const x = random.int(0, 54);
-  const y = random.int(0, 6);
-  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
-
-  const data = {
-    date: date,
-  };
-  console.log(date);
-  jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date },makeCommits.bind(this,--n));
-  });
+// Function to create multiple commits in a row
+const createRow = (week, startDay, endDay) => {
+  for (let day = startDay; day <= endDay; day++) {
+    createCommit(week, day);
+  }
 };
 
-makeCommits(100);
+// Function to create the letter G
+const createG = (startWeek) => {
+  // Top horizontal line
+  createRow(startWeek, 0, 4);
+  // Right vertical line
+  for (let week = startWeek; week <= startWeek + 4; week++) {
+    createCommit(week, 4);
+  }
+  // Bottom horizontal line
+  createRow(startWeek + 4, 0, 4);
+  // Left vertical line
+  for (let week = startWeek; week <= startWeek + 4; week++) {
+    createCommit(week, 0);
+  }
+  // Middle horizontal line
+  createRow(startWeek + 2, 2, 4);
+};
+
+// Function to create the letter A
+const createA = (startWeek) => {
+  // Left diagonal
+  for (let i = 0; i <= 4; i++) {
+    createCommit(startWeek + i, i);
+  }
+  // Right diagonal
+  for (let i = 0; i <= 4; i++) {
+    createCommit(startWeek + i, 4 - i);
+  }
+  // Middle horizontal line
+  createRow(startWeek + 2, 1, 3);
+};
+
+// Create the pattern GAGAN
+const createPattern = () => {
+  // First G
+  createG(0);
+  // First A
+  createA(6);
+  // Second G
+  createG(12);
+  // Second A
+  createA(18);
+  // N
+  for (let i = 0; i <= 4; i++) {
+    createCommit(24 + i, 0);
+    createCommit(24 + i, 4);
+    createCommit(24 + i, i);
+  }
+};
+
+// Execute the pattern creation
+createPattern();
